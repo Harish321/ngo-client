368fce41a049e98b9a5df90e86b9404711d02afb
Harish Dhulipalla <harish.d14@iiits.in>
2023-02-16 17:02:27 +0000

    address update

--- src/components/FootBar.js
+++ src/components/FootBar.js
@@ -4,7 +4,8 @@ const FootBar = () => {
       <div className="footer-content">
         <h4>Address</h4>
         <p>Shivaji Smarak Bhavan, Veer Bahwan Chowk, Panipat, Haryana</p>
-        Queries - atul2464@rediffmail.com
+        Queries - rashtrotthanyas@gmail.com
+        Mobile - 9818501613
       </div>
       <div className="footer-bottom">
         <p>copyright &copy;2022 Rashtrotthan Nyas (Regd.).</p>
